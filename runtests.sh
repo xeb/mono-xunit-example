d68b07cfef5affba9f7dce67b6a25d04c1b4fe06
Mark Kockerbeck <mark@kockerbeck.com>
2014-08-17 21:53:29 -0700

    Trying out appveyor

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -rf test-reports/results.xml
 xbuild
-mono packages/xunit.runners.1.9.2/tools/xunit.console.clr4.x86.exe bin/Debug/MonoXunit.Tests.dll /nunit testresults.xml
+mono --runtime=v4.0.30319 packages/xunit.runners.1.9.2/tools/xunit.console.clr4.x86.exe bin/Debug/MonoXunit.Tests.dll /nunit testresults.xml
 
