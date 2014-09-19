d3231cceba1027080fbb8e57330dc67392c17575
mkockerbeck <mkockerbeck@blizzard.com>
2014-09-18 17:05:45 -0700

    Removing files

--- runtests.sh
+++ runtests.sh
@@ -2,4 +2,3 @@
 rm -rf test-reports/results.xml
 xbuild
 mono --runtime=v4.0.30319 packages/xunit.runners.1.9.2/tools/xunit.console.clr4.x86.exe bin/Debug/MonoXunit.Tests.dll /nunit testresults.xml
-
